ef296575ac57d12e05c802d6ebee706639ade48d
minji <321963741@naver.com>
2022-12-26 09:59:24 +0900

    feat: changes files

--- app.js
+++ app.js
@@ -9,11 +9,6 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
-// swagger
-const swaggerUI=require('swagger-ui-express');
-const YAML =require('yamljs');
-const swaggerDocument=YAML.load('./swagger.yaml');
-
 // connect DB
 const connectDB = require('./db/connect');
 
@@ -39,20 +34,18 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-app.get('/', (req, res) => {
-  res.send('<h1>jobs API</h1><a href="/api-docs">Swagger Documentation</a>');
-});
-app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocument));
-
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
+app.get('/', (req, res) => {
+  res.send('jobs api');
+});
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-//port
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
